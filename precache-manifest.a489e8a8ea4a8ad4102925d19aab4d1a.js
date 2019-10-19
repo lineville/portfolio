5c69dc7bd96589e832d6e37f7c92bf913e3d490a
Liam Neville <lneville@quantumscape.com>
2019-10-18 23:26:13 -0700

    Updates

---
+++ precache-manifest.a489e8a8ea4a8ad4102925d19aab4d1a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd942ce27c749978205c",
+    "url": "/portfolio/static/css/main.d9c0deb0.chunk.css"
+  },
+  {
+    "revision": "fd942ce27c749978205c",
+    "url": "/portfolio/static/js/main.fd942ce2.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.53ebc025.js"
+  },
+  {
+    "revision": "3dda4bdb8e262adc954c",
+    "url": "/portfolio/static/js/2.3dda4bdb.chunk.js"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/portfolio/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/portfolio/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/portfolio/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/portfolio/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/portfolio/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "/portfolio/static/media/square5.ff004a0e.png"
+  },
+  {
+    "revision": "6619b9792d7d108bb589b51bbd560278",
+    "url": "/portfolio/static/media/square2.6619b979.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/portfolio/static/media/square4.8fdf9ee2.png"
+  },
+  {
+    "revision": "23fa76237f0066f4ffd8f20e77449887",
+    "url": "/portfolio/static/media/square3.23fa7623.png"
+  },
+  {
+    "revision": "ca25707397fcc982d7dc1f82555f09a0",
+    "url": "/portfolio/static/media/square1.ca257073.png"
+  },
+  {
+    "revision": "b1e01ebbb0d098277dcc97759bc3f402",
+    "url": "/portfolio/static/media/path1.b1e01ebb.png"
+  },
+  {
+    "revision": "098894095f88df7fdfc4e3d5c0e8679b",
+    "url": "/portfolio/static/media/sensical.09889409.jpeg"
+  },
+  {
+    "revision": "87e8417f8cb5e6eb5ffb8fd3aae3972f",
+    "url": "/portfolio/static/media/nba-playoff-simulator.87e8417f.JPG"
+  },
+  {
+    "revision": "3b9eaea1427b6ec1a98d30eae7804ab3",
+    "url": "/portfolio/static/media/graceshopper-shoelala.3b9eaea1.JPG"
+  },
+  {
+    "revision": "dafb511b07bc1584cdc611b0f017f602",
+    "url": "/portfolio/static/media/get-stuff-done.dafb511b.JPG"
+  },
+  {
+    "revision": "e37326c7e0fd9826c1a35b231340f58b",
+    "url": "/portfolio/static/media/path4.e37326c7.png"
+  },
+  {
+    "revision": "8312982465a792261df84c3de2bc766a",
+    "url": "/portfolio/static/media/liam.83129824.jfif"
+  },
+  {
+    "revision": "8c18abaec52a126a773bd66d0534e9c6",
+    "url": "/portfolio/index.html"
+  }
+];
